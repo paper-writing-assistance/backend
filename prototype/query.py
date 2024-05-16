8a934fcdbc6159d99f384480d51248c2e1e095b5
Jimin Yoo <jmy0819_@naver.com>
2024-05-16 17:49:52 +0900

    feat: Update query to return top K results

--- prototype/query.py
+++ prototype/query.py
@@ -13,10 +13,12 @@ PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_ENV = os.getenv('PINECONE_ENV')
 MONGODB_URI = os.getenv('MONGODB_URI')
 
+NUM_QUERY_RESULTS = 5
+
 # Pinecone client
 pc = Pinecone(api_key=PINECONE_API_KEY)
 index_raw = pc.Index('prototype')
-index_sum = pc.Index('yoso')
+index_sum = pc.Index('dps')
 
 # MongoDB client
 client = MongoClient(MONGODB_URI, server_api=ServerApi('1'))
@@ -44,41 +46,55 @@ while True:
     resp_raw = index_raw.query(
         namespace='raw_v2',
         vector=query_raw,
-        top_k=1,
+        top_k=NUM_QUERY_RESULTS,
     )
 
     resp_summary = index_sum.query(
         namespace='summary',
         vector=query_sum,
-        top_k=1,
+        top_k=NUM_QUERY_RESULTS,
     )
-
     # Result: Raw text
-    result_raw = resp_raw['matches'][0]
-    doc_id_raw = result_raw['id']
-    score_raw = result_raw['score']
-    doc_raw = collection.find_one({ '_id': doc_id_raw })
-
-    # Result: Summary
-    result_summary = resp_summary['matches'][0]
-    doc_id_summary = result_summary['id']
-    score_summary = result_summary['score']
-    doc_summary = collection.find_one({ '_id': doc_id_summary })
-
-    raw_title = (doc_raw['title'] 
-                 if len(doc_raw['title']) < 55 
-                 else doc_raw['title'][:51] + '...')
-    
-    summary_title = (doc_summary['title'] 
-                     if len(doc_summary['title']) < 55 
-                     else doc_summary['title'][:51] + '...')
-
     print()
-    print(f'┌────────────┬────────┬────────────────────────────────────────────────────────┐')
-    print(f'│ Embedding  │ Score  │ Title                                                  │')
-    print(f'├────────────┼────────┼────────────────────────────────────────────────────────┤')
-    print(f'│ Abstract   │ {score_raw:.4f} │ {raw_title:54} │')
-    print(f'├────────────┼────────┼────────────────────────────────────────────────────────┤')
-    print(f'│ Summarized │ {score_summary:.4f} │ {summary_title:54} │')
-    print(f'└────────────┴────────┴────────────────────────────────────────────────────────┘')
-    print('\n'*12)
+    print(f'┌──────────────────────────────────────────────────────────────────────────────┐')
+    print(f'│ {"Abstract": ^76} │')
+    print(f'├────────┬─────────────────────────────────────────────────────────────────────┤')
+    print(f'│ Score  │ Title                                                               │')
+    
+    for i in range(NUM_QUERY_RESULTS):
+        result_raw = resp_raw['matches'][i]
+        doc_id_raw = result_raw['id']
+        score_raw = result_raw['score']
+        doc_raw = collection.find_one({ '_id': doc_id_raw })
+
+        raw_title = (doc_raw['title'] 
+                    if len(doc_raw['title']) < 68 
+                    else doc_raw['title'][:64] + '...')
+        
+        print(f'├────────┼─────────────────────────────────────────────────────────────────────┤')
+        print(f'│ {score_raw:.4f} │ {raw_title.replace('\n', ''):67} │')
+    
+    print(f'└────────┴─────────────────────────────────────────────────────────────────────┘')
+        
+    # Result: Summary
+    print()
+    print(f'┌──────────────────────────────────────────────────────────────────────────────┐')
+    print(f'│ {"Domain, Problem, Summary": ^76} │')
+    print(f'├────────┬─────────────────────────────────────────────────────────────────────┤')
+    print(f'│ Score  │ Title                                                               │')
+    
+    for i in range(NUM_QUERY_RESULTS):
+        result_summary = resp_summary['matches'][i]
+        doc_id_summary = result_summary['id']
+        score_summary = result_summary['score']
+        doc_summary = collection.find_one({ '_id': doc_id_summary })
+        
+        summary_title = (doc_summary['title'] 
+                        if len(doc_summary['title']) < 68 
+                        else doc_summary['title'][:64] + '...')
+        
+        print(f'├────────┼─────────────────────────────────────────────────────────────────────┤')
+        print(f'│ {score_summary:.4f} │ {summary_title.replace('\n', ''):67} │')
+
+    print(f'└────────┴─────────────────────────────────────────────────────────────────────┘')
+        
