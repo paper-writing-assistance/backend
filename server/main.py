7bf85946595c18b9ff718bc89225e1870aaa8d11
Jimin Yoo <jmy0819_@naver.com>
2024-05-19 12:09:56 +0900

    feat: Return score values for similarity ranking

--- server/main.py
+++ server/main.py
@@ -23,3 +23,22 @@ async def search(domain: str, problem: str, solution: str):
     return {
         "results": results
     }
+
+
+@app.get("/similarity")
+async def similarity(domain: str, problem: str, solution: str, k: int):
+    ids = [
+        "59b63f10-57e0-4f1d-a013-32c141e196cd",
+        "d31b2761-7b9f-44c5-9df3-51d5c612296d",
+        "d18dfbc2-9125-461a-9951-fa844a19e91c",
+        "464956be-8b26-4fc2-a974-e33b37e23d19",
+    ]
+
+    query = vector.create_embedding(domain, problem, solution)
+    targets = vector.fetch_all_by_id(ids)
+
+    result = vector.rank_by_similarity(query, targets, k)
+
+    return {
+        "result": result
+    }
