8ddb212364b59e382740430f88a7eb8d699b26eb
Jimin Yoo <jmy0819_@naver.com>
2024-05-30 15:34:31 +0900

    chore: Remove unused codes

--- server/main.py
+++ server/main.py
@@ -3,79 +3,22 @@ import json
 from fastapi import FastAPI
 from server.database import vector, document, graph
 from pydantic import BaseModel
-from .routers import search
+from .routers import search, paper
 
-app = FastAPI()
+app = FastAPI(
+    title="YOSO API Documentation",
+    version="0.2.0"
+)
 app.include_router(
     router=search.router,
     prefix="/api/v1"
 )
+app.include_router(
+    router=paper.router,
+    prefix="/api/v1"
+)
 
 
-@app.get("/ping")
+@app.get("/ping", summary="접속 테스트")
 def read_root():
     return "pong"
-
-
-class SearchBody(BaseModel):
-    domain: str
-    problem: str
-    solution: str
-
-
-@app.post("/search", summary="핵심 페이퍼 도출")
-async def search(body: SearchBody):
-    # Retrieve top 5 relevant paper ids
-    doc_ids = vector.search_by_sentence(
-        domain=body.domain, 
-        problem=body.problem,
-        solution=body.solution, 
-        k=5
-    )
-
-    # Fetch title, year, keywords from document database
-    docs = [document.search_by_id(id) for id in doc_ids]
-    
-    results = [{
-        "id": doc["_id"],
-        "title": doc["title"].replace("-\n", "").replace("\n", " "),
-        "published_year": doc["published_year"],
-        "keywords": doc["keywords"]
-    } for doc in docs]
-
-    return results
-
-
-@app.get("/reset")
-def hard_reset_database():
-    try:
-        with open("server/merged.json", "r") as file:
-            data = json.load(file)
-            for elem in data:
-                # MongoDB
-                document.create_document(**elem)
-
-                # Vector
-                # vector.create_vector(
-                #     id=elem["id"],
-                #     domain=elem["summary"]["domain"],
-                #     problem=elem["summary"]["problem"],
-                #     solution=elem["summary"]["solution"]
-                # )
-
-                # Graph
-                # graph.create_node(
-                #     # driver=driver, 
-                #     id=elem["id"],
-                #     title=elem["title"]
-                # )
-
-                # for ref_title in elem["reference"]:
-                #     graph.create_relationship(
-                #         # driver=driver,
-                #         title=elem["title"],
-                #         ref_title=ref_title
-                #     )
-        return "Success"
-    except Exception as e:
-        return f"Failed: {e}"
