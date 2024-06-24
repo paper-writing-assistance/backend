e4a4a2b886ab07aa1cd23689bea1e72389513c29
Jimin Yoo <jmy0819_@naver.com>
2024-06-24 15:33:07 +0900

    feat: add s3 upload utils

--- app/core/config.py
+++ app/core/config.py
@@ -34,6 +34,10 @@ class Settings(BaseSettings):
 
     PINECONE_API_KEY: str
     PINECONE_INDEX: str
+
+    AWS_ACCESS_KEY_ID: str
+    AWS_SECRET_ACCESS_KEY: str
+    S3_BUCKET_NAME: str
     
 
     @computed_field
