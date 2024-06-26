ad5d52d9b198d0169bf4ea4470f2a6e4736abb09
Jimin Yoo <jmy0819_@naver.com>
2024-06-26 14:26:04 +0900

    fix: fix cors origin and dependencies

--- app/core/config.py
+++ app/core/config.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
     PROJECT_NAME: str
     API_V1_STR: str = "/api/v1"
 
-    BACKEND_CORS_ORIGINS: list[AnyUrl] | str = []
+    BACKEND_CORS_ORIGINS: list[AnyUrl] | str = ["*"]
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
